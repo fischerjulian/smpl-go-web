f2459987d12456aea77cc025dd0f00cbb2ac490c
Julian Fischer <julian.fischer@anynines.com>
2020-02-21 11:49:26 +0100

    Green

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	// Method: GET
 	// Resource http://localhost:8080
 	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.HTML("<h1>Stupid but Funny</h1>")
+		ctx.HTML("<h1 style=\"color:green;\">I am green.</h1>")
 	})
 
 	// same as app.Handle("GET", "/ping", ..)
